17155f279b82a438b409dfa8512b76ac4014f74e
Anthonydt16 <anthonydt16@gmail.com>
2025-08-28 21:38:18 +0200

    fix: Update loading state handling and improve post rendering logic in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,19 @@ import Loading from "@/components/atom/loading";
 export default function Home() {
   // Fetch Instagram posts data
   const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchPosts = async () => {
       fetch("/api/post?pinned=true")
         .then((res) => res.json())
         .then((instaPosts) => {
           setPosts(instaPosts);
+          setLoading(false);
         });
     };
     fetchPosts();
   }, []);
-  if (!posts || posts.length === 0) {
+  if (loading) {
     return <Loading />;
   }
   return (
@@ -29,30 +31,39 @@ export default function Home() {
         Découvrez les dernières tendances du rap et de la culture urbaine.
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {posts.map((post) => {
-          // Ensure postContent is the expected object type
-          const postContent =
-            typeof post.postContent === "object" && post.postContent !== null
-              ? (post.postContent as { caption?: string; image_url?: string[] })
-              : {};
-          return (
-            <PostAffiche
-              key={post.id}
-              post={{
-                id: post.id,
-                postContent: {
-                  caption: postContent.caption ?? "Aucun texte disponible",
-                  image_url: postContent.image_url?.[0]
-                    ? `/api/proxy?url=${encodeURIComponent(
-                        postContent.image_url?.[0] ?? ""
-                      )}`
-                    : "/default-image.png",
-                  url: "post/" + post.id,
-                },
-              }}
-            />
-          );
-        })}
+        {posts.length === 0 ? (
+          <div className="col-span-4 text-center text-gray-500">
+            Aucun post épinglée disponible
+          </div>
+        ) : (
+          posts.map((post) => {
+            // Ensure postContent is the expected object type
+            const postContent =
+              typeof post.postContent === "object" && post.postContent !== null
+                ? (post.postContent as {
+                    caption?: string;
+                    image_url?: string[];
+                  })
+                : {};
+            return (
+              <PostAffiche
+                key={post.id}
+                post={{
+                  id: post.id,
+                  postContent: {
+                    caption: postContent.caption ?? "Aucun texte disponible",
+                    image_url: postContent.image_url?.[0]
+                      ? `/api/proxy?url=${encodeURIComponent(
+                          postContent.image_url?.[0] ?? ""
+                        )}`
+                      : "/default-image.png",
+                    url: "post/" + post.id,
+                  },
+                }}
+              />
+            );
+          })
+        )}
       </div>
     </div>
   );
